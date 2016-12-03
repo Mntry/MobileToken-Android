3f4d532b52f57f0427d5595a9b06ebb3f4688bd2
Bobby Bonestell <rbonestell@gmail.com>
2016-12-03 12:03:44 -0700

    Added tokenization result handler to demo app

--- MonetaryMobileTokenDemo/app/src/main/java/co/monetary/mobiletokendemo/MainActivity.java
+++ MonetaryMobileTokenDemo/app/src/main/java/co/monetary/mobiletokendemo/MainActivity.java
@@ -1,5 +1,6 @@
 package co.monetary.mobiletokendemo;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == MonetaryTokenizerActivity.MONETARY_TOKENIZER_REQUEST)
+        {
+            if (resultCode == MonetaryTokenizerActivity.RESULT_SUCCESS)
+            {
+                MonetaryToken token = (MonetaryToken)data.getSerializableExtra("token");
+                new AlertDialog.Builder(this)
+                        .setTitle("Token Response")
+                        .setMessage("Token: " + token.token)
+                        .setNeutralButton("OK", null).show();
+            }
+            else if (resultCode == MonetaryTokenizerActivity.RESULT_ERROR)
+            {
+                MonetaryTokenizationError error = (MonetaryTokenizationError)data.getSerializableExtra("error");
+                new AlertDialog.Builder(this)
+                        .setTitle("Tokenization Error")
+                        .setMessage("Error Code: " + error.errorCode.toString() + "\nError Message: " + error.errorMessage)
+                        .setNeutralButton("OK", null).show();
+
+            }
+            else if (resultCode == MonetaryTokenizerActivity.RESULT_CANCELED)
+            {
+                new AlertDialog.Builder(this)
+                        .setTitle("Tokenization Cancelled")
+                        .setMessage("Tokenization Cancelled")
+                        .setNeutralButton("OK", null).show();
+
+            }
+        }
     }
 
     public void getTokenPressed(View v) {
