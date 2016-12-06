d4195fee813b1495ba9f21fa5ffd58990431292c
Bobby Bonestell <rbonestell@gmail.com>
2016-12-06 15:50:52 -0700

    Updated UI and added logo

--- app/src/main/java/co/monetary/mobiletokendemo/MainActivity.java
+++ app/src/main/java/co/monetary/mobiletokendemo/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import co.monetary.mobiletoken.*;
 
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
